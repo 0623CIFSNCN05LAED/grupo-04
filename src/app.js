cf055cc04238319fad277b2c8880f523a8950f98
azuuvp24 <azulvp24@gmail.com>
2023-09-05 15:35:51 -0300

    Agregando las primeras modificaciones para el modelo EJS

--- src/app.js
+++ src/app.js
@@ -4,19 +4,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "../public")))
 
-<<<<<<< HEAD
-const PORT = 3002;
-app.listen(PORT, ()=> {
-    console.log(`Puerto ${PORT} encendido`)
-})
-
-app.get("/login", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/login.html"))
-})
-=======
 const PORT = 3001;
 app.listen(process.env.PORT || PORT, () => {
-    console.log("Server en puerto 3001");
+    console.log(`Server en puerto ${PORT}`);
 });
 
 app.get("/", (req, res) => {
@@ -31,4 +21,6 @@ app.get("/detalles-de-producto", (req,res)=>{
     res.sendFile(path.join(__dirname, "views/productDetails.html"))
 })
 
->>>>>>> ed461d630809d20cefdd78d32afa15360c0e744b
+app.get("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
