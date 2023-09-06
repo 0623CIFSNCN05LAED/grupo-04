935f2a8fe23080bd5db8df6cb3d46edf1e4f1b72
nadiiasoria <nadiasoria5972@gmail.com>
2023-09-05 23:03:22 -0300

    primer commit

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const path = require("path")
 const app = express();
 
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"))
+
 app.use(express.static(path.join(__dirname, "../public")))
 
 const PORT = 3001;
